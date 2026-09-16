bf80b99336b21ff2661e20a4283ae91cdd6d21e3
Tonyynf <tobyencarnacao@gmail.com>
2026-09-16 19:17:21 -0300

    feat: add routes to the LivroController

--- LibraryManager/src/main/java/com/example/LibraryManager/controllers/LivroController.java
+++ LibraryManager/src/main/java/com/example/LibraryManager/controllers/LivroController.java
@@ -1,4 +1,29 @@
 package com.example.LibraryManager.controllers;
 
+import com.example.LibraryManager.models.Livro;
+import com.example.LibraryManager.services.LivroService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/livros")
+@RequiredArgsConstructor
 public class LivroController {
+    private final LivroService livroService;
+
+    @GetMapping
+    public ResponseEntity<List<Livro>> getAllLivros(){
+        return ResponseEntity.ok(livroService.listarTodos());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> getLivroById(@PathVariable Long id){
+        return ResponseEntity.ok(livroService.buscarPorId(id));
+    }
+    @GetMapping("/{titulo}")
+    public ResponseEntity<List<Livro>> getAllLivrosPorTitulo(String titulo){
+        return ResponseEntity.ok(livroService.buscarPorTitulo(titulo));
+    }
 }
