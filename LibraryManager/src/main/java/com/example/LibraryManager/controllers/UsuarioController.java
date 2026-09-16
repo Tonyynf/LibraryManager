addfc4fd31ae409564c0ddf3189eec5fd7371e94
Tonyynf <tobyencarnacao@gmail.com>
2026-09-16 19:16:58 -0300

    feat: add routes to the UsuarioController

--- LibraryManager/src/main/java/com/example/LibraryManager/controllers/UsuarioController.java
+++ LibraryManager/src/main/java/com/example/LibraryManager/controllers/UsuarioController.java
@@ -1,4 +1,38 @@
 package com.example.LibraryManager.controllers;
 
+import com.example.LibraryManager.models.Usuario;
+import com.example.LibraryManager.services.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAllUsuarios(){
+        return ResponseEntity.ok(usuarioService.buscarTodos());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id){
+        return ResponseEntity.ok(usuarioService.buscarPorId(id));
+    }
+    @PostMapping
+    public ResponseEntity<Usuario> createUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(usuarioService.criarUsuario(usuario));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> updateUsuario(@PathVariable Long id, @RequestBody Usuario usuario){
+        return ResponseEntity.ok(usuarioService.atualizarUsuario(id, usuario));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Usuario> deleteUsuario(@PathVariable Long id){
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.ok().build();
+    }
 }
