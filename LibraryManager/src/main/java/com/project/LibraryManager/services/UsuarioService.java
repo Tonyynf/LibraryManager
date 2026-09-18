750586fc6cdb0ee9565c72c23c86c5f392529a09
Tonyynf <tobyencarnacao@gmail.com>
2026-09-18 15:26:01 -0300

    refactor: changes name of UsuarioRepository for usuarioRepository to the good practices

--- LibraryManager/src/main/java/com/project/LibraryManager/services/UsuarioService.java
+++ LibraryManager/src/main/java/com/project/LibraryManager/services/UsuarioService.java
@@ -9,24 +9,24 @@ import java.util.List;
 
 @Service
 public class UsuarioService {
-    private final UsuarioRepository UsuarioRepository;
+    private final UsuarioRepository usuarioRepository;
 
     public UsuarioService(UsuarioRepository usuarioRepository) {
-        this.UsuarioRepository = usuarioRepository;
+        this.usuarioRepository = usuarioRepository;
     }
 
     public Usuario buscarPorId(Long id){
-        return UsuarioRepository.findById(id)
+        return usuarioRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
     }
 
     public List<Usuario> buscarTodos(){
-        return UsuarioRepository.findAll();
+        return usuarioRepository.findAll();
     }
 
     public UsuarioResponseDTO criarUsuario(UsuarioRequestDTO usuario){
         //↓ ↓ ↓ Depois criar um package ou arquivo de exceptions ↓ ↓ ↓
-        boolean emailExistente = UsuarioRepository.existsByEmail(usuario.email());
+        boolean emailExistente = usuarioRepository.existsByEmail(usuario.email());
 
         if(emailExistente){
             throw new RuntimeException("Este email já está cadastrado!");
@@ -39,27 +39,27 @@ public class UsuarioService {
                 usuario.senha()
         );
 
-        return converterParaResponseDto(UsuarioRepository.save(usuarioNovo));
+        return converterParaResponseDto(usuarioRepository.save(usuarioNovo));
     }
 
     public UsuarioResponseDTO atualizarUsuario(Long id, UsuarioRequestDTO usuarioAtualizado){
-        return UsuarioRepository.findById(id).map(usuarioExistente -> {
+        return usuarioRepository.findById(id).map(usuarioExistente -> {
                     usuarioExistente.setNome(usuarioAtualizado.nome());
                     usuarioExistente.setEmail(usuarioAtualizado.email());
                     //Talvez essaa alteração de senha precise passar pelo HashCode
                     usuarioExistente.setSenha(usuarioAtualizado.senha());
 
-                    return converterParaResponseDto(UsuarioRepository.save(usuarioExistente));
+                    return converterParaResponseDto(usuarioRepository.save(usuarioExistente));
             //↓ ↓ ↓ Depois criar um package ou arquivo de exceptions ↓ ↓ ↓
         }).orElseThrow(() -> new RuntimeException("Usuario não encontrado!"));
     }
 
     public void deletarUsuario(Long id){
-        if(!UsuarioRepository.existsById(id)){
+        if(!usuarioRepository.existsById(id)){
             //↓ ↓ ↓ Depois criar um package ou arquivo de exceptions ↓ ↓ ↓
             throw new RuntimeException("Não é possivel exclui. Usuário não foi encontrado!");
         }
-        UsuarioRepository.deleteById(id);
+        usuarioRepository.deleteById(id);
     }
 
     private UsuarioResponseDTO converterParaResponseDto(Usuario user) {
